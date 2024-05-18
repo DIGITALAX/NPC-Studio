7551f17a3a6efbc635fb708d7d456787555cba1b
emmajane1313 <admin@digitalax.xyz>
2024-05-18 02:57:39 -0400

    cambiar idi

--- src/components/layout/modules/Footer.tsx
+++ src/components/layout/modules/Footer.tsx
@@ -3,16 +3,63 @@ import Image from "next/legacy/image";
 import { ReactElement } from "react";
 import { BsTwitter } from "react-icons/bs";
 import { INFURA_GATEWAY } from "../../../lib/constants";
+import {
+  PiArrowFatLinesLeftFill,
+  PiArrowFatLinesRightFill,
+} from "react-icons/pi";
+import { usePathname, useRouter } from "next/navigation";
 
 function Footer() {
+  const router = useRouter();
+  const path = usePathname();
   return (
     <div className="relative w-full h-fit py-4 flex items-center justify-between gap-6 flex-col sm:flex-row mb-0 px-4">
-      <div
-        className="relative w-fit h-fit flex items-center justify-center ml-0 font-rain text-white text-4xl cursor-pointer active:scale-95"
-        onClick={() => window.open("https://www.digitalax.xyz/")}
-      >
-        DIGITALAX
+      <div className="relative w-fit h-fit flex items-center justify-center gap-4 flex-row">
+        <div
+          className="relative w-fit h-fit flex items-center justify-center ml-0 font-rain text-white text-4xl cursor-pointer active:scale-95"
+          onClick={() => window.open("https://www.digitalax.xyz/")}
+        >
+          DIGITALAX
+        </div>
+        <div className="relative w-fit h-fit flex items-center justify-center flex-row gap-2">
+          <div
+            className="relative flex items-center justify-center w-fit h-fit active:scale-95 cursor-pointer"
+            onClick={() =>
+              router.push(
+                path.includes("/en/")
+                  ? path.replace("/en/", "/es/")
+                  : path.replace("/es/", "/en/")
+              )
+            }
+          >
+            <PiArrowFatLinesLeftFill size={15} color={"white"} />
+          </div>
+          <div className="relative flex items-center justify-center w-10 h-9">
+            <Image
+              draggable={false}
+              layout="fill"
+              src={`${INFURA_GATEWAY}/ipfs/${
+                path.includes("/en/")
+                  ? "QmWDGpLPFi3cejfF3uBdFfjE9v2yeyFRShn5DQhiKXgDdo"
+                  : "QmNTNfAqo6v4T1fQc8YVPAnKmLTVdyripzBYyLc6HXMSEA"
+              }`}
+            />
+          </div>
+          <div
+            className="relative flex items-center justify-center w-fit h-fit active:scale-95 cursor-pointer"
+            onClick={() =>
+              router.push(
+                path.includes("/en/")
+                  ? path.replace("/en/", "/es/")
+                  : path.replace("/es/", "/en/")
+              )
+            }
+          >
+            <PiArrowFatLinesRightFill size={15} color={"white"} />
+          </div>
+        </div>
       </div>
+
       <div className="relative w-fit h-fit flex items-center justify-center mr-0">
         <div className="relative w-fit h-fit mr-0 flex-row gap-3 flex items-center justify-center md:pr-2 md:order-3 order-1">
           {[
