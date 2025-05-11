f6f1fec87aceb11be460063fae35d5f98b47b008
emmajane1313 <emma.jane@digitalax.xyz>
2025-05-11 18:22:45 -0400

    image sitemap

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -16,11 +16,11 @@ export async function GET() {
         ${locales
           .map(
             (locale) => `
-          <link rel="alternate" hreflang="${locale}" href="${baseUrl}/${locale}/" ></link>
+          <xhtml:link rel="alternate" hreflang="${locale}" href="${baseUrl}/${locale}/" />
           `
           )
           .join("")}
-        <link rel="alternate" hreflang="x-default" href="${baseUrl}/"  ></link>
+        <xhtml:link rel="alternate" hreflang="x-default" href="${baseUrl}/"  />
       </url>
 
       <url>
@@ -28,11 +28,11 @@ export async function GET() {
         ${locales
           .map(
             (locale) => `
-          <link rel="alternate" hreflang="${locale}" href="${baseUrl}/${locale}/agent-index/"  ></link>
+          <xhtml:link rel="alternate" hreflang="${locale}" href="${baseUrl}/${locale}/agent-index/" />
           `
           )
           .join("")}
-        <link rel="alternate" hreflang="x-default" href="${baseUrl}/agent-index/"  ></link>
+        <xhtml:link rel="alternate" hreflang="x-default" href="${baseUrl}/agent-index/"  />
       </url>
 
      
